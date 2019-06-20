a9d1e58c09fd72846b9d9efc76c420885402ad4e
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-19 20:26:41 -0400

    Added Latestes Close and Latest Closing Price

--- robo-advisor.py
+++ robo-advisor.py
@@ -1,5 +1,31 @@
+import json
 import os
-import pandas
+import requests
+
+#from dotenv import load_dotenv
+
+
+
+#load_dotenv() #> loads contents of the .env file into the script's environment
+
+API_KEY = os.environ.get("9437TKPHKRA9TX6O") #Remove API Key later
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+
+response=requests.get(request_url)
+#print(type(response)) 
+#print(response.status_code)
+#print(response.text)
+
+parsed_response = json.loads(response.text) #convert str to dic
+
+#breakpoint ()
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"]["2019-06-19"]["4. close"]
+
+def to_usd (my_price):
+    return "${0:0.2f}".format(my_price)
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -7,8 +33,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
