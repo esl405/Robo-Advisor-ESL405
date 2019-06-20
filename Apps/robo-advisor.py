04ea543d943e44bcd0818bedcd73776cfd90319c
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-19 21:19:19 -0400

    shuffled files and added csv file function

--- Apps/robo-advisor.py
+++ Apps/robo-advisor.py
@@ -1,6 +1,9 @@
 import json
 import os
 import requests
+import datetime
+import pandas
+import csv
 
 #from dotenv import load_dotenv
 
@@ -45,6 +48,22 @@ for date in dates:
 
 recent_low = min(low_prices)
 
+#formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
+
+#Convert to CSV
+
+#csv_file_path = "data/prices.csv" # a relative filepath
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "Data", "monthly_sales.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 def to_usd (my_price):
     return "${0:0.2f}".format(my_price)
 
@@ -52,7 +71,7 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+#print(f"REQUESTED AT: {formatted_time_now}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
@@ -61,6 +80,8 @@ print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
